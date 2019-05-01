1b72986da57468a97fcc1b86d1e30a4721a4d7e2
Clyde McQueen <clyde@mcqueen.net>
2019-05-01 16:07:17 -0700

    Default for UP board is bus=5

--- example.py
+++ example.py
@@ -3,11 +3,11 @@ from __future__ import print_function
 import ms5837
 import time
 
-sensor = ms5837.MS5837_30BA() # Default I2C bus is 1 (Raspberry Pi 3)
+#sensor = ms5837.MS5837_30BA() # Default I2C bus is 1 (Raspberry Pi 3)
 #sensor = ms5837.MS5837_30BA(0) # Specify I2C bus
 #sensor = ms5837.MS5837_02BA()
 #sensor = ms5837.MS5837_02BA(0)
-#sensor = ms5837.MS5837(model=ms5837.MS5837_MODEL_30BA, bus=0) # Specify model and bus
+sensor = ms5837.MS5837(model=ms5837.MODEL_30BA, bus=5) # Specify model and bus
 
 # We must initialize the sensor before reading it
 if not sensor.init():
